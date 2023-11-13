27726776efab004338ebe9be058a6df37082290d
jaime <silvacastillojaime@gmail.com>
2023-11-13 16:32:02 -0300

    corrige ortografia

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -38,7 +38,7 @@ const ListadoPacientes = ({ pacientes, setPaciente, eliminarPaciente }) => {
             NO hay pacientes
             </h2>
             <p className=" text-xl mt-5 mb-10 text-center">
-            Comienza agregando pacientes {''} <span className=" text-indigo-600 font-bold" >y pareceran en este lugar </span>
+            Comienza agregando pacientes {''} <span className=" text-indigo-600 font-bold" >y apareceran en este lugar </span>
             </p>
           </>
       )}
